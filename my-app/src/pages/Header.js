f8bbdb1fdb06cbe6007f64836f5dfe9197c36c46
anjali <anjalimarella3@gmail.com>
2021-11-03 17:47:25 +0530

    added a comment

--- my-app/src/pages/Header.js
+++ my-app/src/pages/Header.js
@@ -2,26 +2,29 @@ import { Navbar, NavbarBrand, NavItem, NavLink, Collapse } from "reactstrap"
 
 
 const image = "https://res.cloudinary.com/ddcycgbwg/image/upload/v1635920933/logo_agxsqh.jpg"
-
+//Header page
 const Header = () => {
 
     return (
-        <div>
-            <Navbar color="light" light expand="md">
+        <div className="container">
+            <Navbar color="light" light expand="md" className="navbar">
                 <div>
-                    <NavbarBrand href="/"><img src={image} /></NavbarBrand>
+                    <NavbarBrand href="/"><img src={image} className="logo" clat="logo" /></NavbarBrand>
                 </div>
                 <div>
                 </div>
                 <div>
-                    <div>
-                        <NavLink href="/Login">Login</NavLink>
-                        <NavLink href="/Sign-up">Sign up</NavLink>
+                    <div className="navlink-container">
+                        <div>
+                            <NavLink href="/Login" className="nav-link">Login</NavLink>
+                        </div><div>
+                            <NavLink href="/Sign-up" className="nav-link">Sign up</NavLink>
+                        </div>
                     </div>
                 </div>
-            </Navbar>
+            </Navbar >
 
-        </div>
+        </div >
     )
 }
 export default Header
--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -1,8 +1,8 @@
 const Home = () => {
     return (
         <div>
-            <h1>Welcome to innomick</h1>
-            <p>Practices, digital Transformation, Engaging Digital Solutions, Real Time Data and Insights, Enterprise devOps</p>
+            <h1 className="welcome-message">Welcome to innomick</h1>
+            <p className="description">Practices, digital Transformation, Engaging Digital Solutions, Real Time Data and Insights, Enterprise devOps</p>
         </div>
     )
 }
