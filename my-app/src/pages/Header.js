788b543859454be38916af6f95689bfdff76e417
anjali <anjalimarella3@gmail.com>
2021-11-03 20:38:27 +0530

    project-set-up version 2

--- my-app/src/pages/Header.js
+++ my-app/src/pages/Header.js
@@ -1,27 +1,34 @@
 import { Navbar, NavbarBrand, NavItem, NavLink, Collapse } from "reactstrap"
 
+import logo from "../assets/images/logo.jpg"
 
-const image = "https://res.cloudinary.com/ddcycgbwg/image/upload/v1635920933/logo_agxsqh.jpg"
-
+// const image = "https://res.cloudinary.com/ddcycgbwg/image/upload/v1635920933/logo_agxsqh.jpg"
+//Header page
 const Header = () => {
 
     return (
-        <div>
-            <Navbar color="light" light expand="md">
+        <div className="container">
+            <Navbar color="light" light expand="md" className="navbar">
                 <div>
-                    <NavbarBrand href="/"><img src={image} /></NavbarBrand>
+                    <NavbarBrand href="/"><img src={logo} className="logo" alt="logo" /></NavbarBrand>
                 </div>
                 <div>
                 </div>
                 <div>
+                    <div className="navlink-container">
+                        <div>
+                            <NavLink href="/Login" className="nav-link">Login</NavLink>
+                        </div><div>
+                            <NavLink href="/Sign-up" className="nav-link">Sign up</NavLink>
+                        </div>
+                    </div>
                     <div>
-                        <NavLink href="/Login">Login</NavLink>
-                        <NavLink href="/Sign-up">Sign up</NavLink>
+
                     </div>
                 </div>
-            </Navbar>
+            </Navbar >
 
-        </div>
+        </div >
     )
 }
 export default Header
