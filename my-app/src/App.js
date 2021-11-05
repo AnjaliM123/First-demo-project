16b66a7876476947f7e8d988a88eb802f57364dc
anjali <anjalimarella3@gmail.com>
2021-11-05 13:16:28 +0530

    removed unnecessary comments and files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 
 import Header from "./pages/Header"
 import Home from "./pages/index"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+
+
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
