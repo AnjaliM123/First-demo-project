0016deae0b52f36ac207ad1da9a6b21e62c91ba5
anjali <anjalimarella3@gmail.com>
2021-11-08 12:29:52 +0530

    file name changed from ReduxField to ReduxFields

--- src/constants/Validate.js
+++ src/constants/Validate.js
@@ -5,7 +5,7 @@ export const required = value => (value ? undefined : 'Required')
 
 
 
-export const emailValidate = value =>
+export const ValidateEmail = value =>
     value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
         ? 'Invalid email address'
         : null
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
 import { Button, Form } from "reactstrap";
-import { required, emailValidate, maxLength15, minLength6 } from "../constants/Validate"
-import { renderTextField } from "../common/ReduxFileds"
-import { Col } from "reactstrap"
+import { required, validateEmail, maxLength15, minLength6 } from "../constants/Validate"
+import { renderTextField } from "../common/ReduxFields"
 import { Link } from "react-router-dom"
 
 
@@ -17,54 +16,43 @@ const SignUpPage = (props) => {
     const { handleSubmit } = props;
 
     return (
-        <div className="container">
-            <div className="signup-form">
 
-                <h2>Sign Up</h2>
-                <Link to="/login" className="link">Have an account?</Link>
-                <div classNameo="reduxcardcntainer">
-                    <Form onSubmit={handleSubmit(onSubmit)}>
-                        <div className="signup-container">
-                            <Col xs={12} sm={5}>
-                                <Field
-                                    placeholder="username"
-                                    name="username"
-                                    type="text"
-                                    component={renderTextField}
-                                    validate={required}
-                                />
-                            </Col>
-                            <Col xs={12} sm={5}>
-                                <Field
-                                    placeholder="Email"
-                                    name="email"
-                                    type="text"
-                                    component={renderTextField}
-                                    validate={[emailValidate, required]}
-                                />
-                            </Col>
-                            <Col xs={12} sm={5}>
-                                <Field
-                                    placeholder="Password"
-                                    name="password"
-                                    type="password"
-                                    component={renderTextField}
-                                    validate={[required, minLength6, maxLength15]}
-                                />
-                            </Col>
-                        </div>
-                        <div className="button-container">
-                            <div>
-                                <Button type="submit" className="signup-button">
-                                    Sign Up
-                                </Button>
-                            </div>
-                        </div>
-                    </Form>
+        <div className="row col-12 col-sm-7 col-md-5 col-lg-4 d-flex flex-row justify-content-center m-auto pt-5">
+            <h2>Sign Up</h2>
+            <Link to="/login" className="link">Have an account?</Link>
 
+            <Form onSubmit={handleSubmit(onSubmit)}>
+                <Field
+                    placeholder="Email"
+                    name="email"
+                    type="text"
+                    component={renderTextField}
+                    validate={[validateEmail, required]}
+                />
+                <Field
+                    placeholder="Password"
+                    name="password"
+                    type="password"
+                    component={renderTextField}
+                    validate={[required]}
+                />
+                <Field
+                    placeholder="Password"
+                    name="password"
+                    type="password"
+                    component={renderTextField}
+                    validate={[required, minLength6, maxLength15]}
+                />
+                <div className="d-flex flex-row justify-content-end mt-3">
+                    <Button type="submit" className="button">
+                        Sign Up
+                    </Button>
                 </div>
-            </div>
+
+            </Form>
         </div>
+
+
     )
 }
 
