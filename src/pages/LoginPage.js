12fe40a4d3ed84b0defe8f6e1f78ed155fe2467e
anjali <anjalimarella3@gmail.com>
2021-11-10 20:29:16 +0530

    added a functionality to change the header

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -10,13 +10,17 @@ import { useHistory } from "react-router-dom";
 import { Spinner } from "reactstrap";
 import showSuccessMessage from "../redux/helper/alerts";
 import { isUserAuthenticated } from "../redux/helper/authHelper";
-
+import { useState } from "react";
 const LoginPage = (props) => {
   const history = useHistory();
   const dispatch = useDispatch();
+  const [cardAnimaton, setCardAnimation] = useState("cardHidden");
+  const { handleSubmit, reset } = props;
   /*----------on form submit -----------*/
   const onSubmit = (formProps) => {
     dispatch(login({ user: formProps }));
+  reset()
+
   };
 
   const nextProps = useSelector((state) => ({
@@ -24,6 +28,12 @@ const LoginPage = (props) => {
     isAuthenticated: state.users?.isAuthenticated,
     isUserAuthenticated: state.users && state.users.loginData?.response?.data,
   }));
+
+
+  useEffect(() => {
+   
+  });
+  
 
   const FirstRef = useRef(true);
   useEffect(() => {
@@ -33,12 +43,14 @@ const LoginPage = (props) => {
     }
     if (nextProps.isAuthenticated) {
       history.push("/");
+      window.location.reload(false);
       showSuccessMessage("you have loggedin successfully");
+     
     }
-    reset();
+  
   }, [nextProps.isAuthenticated, history]);
 
-  const { handleSubmit, reset } = props;
+
 
   return (
     <div className="row col-12 col-sm-7 col-md-5 col-lg-4 d-flex flex-row justify-content-center m-auto pt-5">
@@ -48,7 +60,7 @@ const LoginPage = (props) => {
       </Link>
 
       {nextProps.isUserAuthenticated && nextProps.isUserAuthenticated ? (
-        <p className="autherror">invalid email or password</p>
+        <p className="autherrorlogin">invalid email or password</p>
       ) : (
         ""
       )}
